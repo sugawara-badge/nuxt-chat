4b895caecf058e6b062fceab14526cf64bf7242a
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-06-12 12:01:22 +0900

    チャット画面へ遷移

--- nuxt-app/components/UserList.vue
+++ nuxt-app/components/UserList.vue
@@ -1,13 +1,22 @@
+<script setup>
+const users = [
+  {id: 1, name: '1郎'},
+  {id: 2, name: '2郎'},
+  {id: 3, name: '3郎'},
+]
+</script>
+
 <template>
   <div class="user-list">
-    <h2 class="text-xl pt-2 pb-2">ユーザー一覧</h2>
+    <h2 class="text-xl pt-4 pb-4">ユーザー一覧</h2>
     <ul>
-      <a href="">
-        <li>一郎</li>
-      </a>
-      <a href="">
-        <li>二郎</li>
-      </a>
+      <li v-for="user in users" class="">
+        <NuxtLink
+          :to="{ name: 'chats-id', params: { id: user.id } }"
+          class="inline-block p-2"
+          >{{ user.name }}</NuxtLink>
+        <hr>
+      </li>
     </ul>
   </div>
 </template>
--- nuxt-app/pages/chat.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="chat">
-    <h2 class="text-xl pt-2 pb-2">チャット</h2>
-  </div>
-</template>
