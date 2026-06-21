31fca25516fc8881c0347f672ebb7610e4033b25
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-06-21 20:01:08 +0900

    修正

---
+++ nuxt-app/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1>チャ。</h1>
+    <p><span>ー</span>地球の運動と腸内環境、及び発毛について<span>ー</span></p>
+  </header>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+  font-family: serif;
+}
+header h1 {
+  font-size: 200px;
+  padding-left: 120px;
+}
+header p {
+  letter-spacing: 1px;
+}
+
+header span {
+  font-family: sans-serif;
+}
+
+@media screen and (max-width: 768px) {
+  header {
+    padding-top: 50px;
+  }
+  header h1 {
+    font-size: 100px;
+    padding-left: 50px;
+  }
+}
+header p {
+  font-size: 10px;
+}
+</style>
