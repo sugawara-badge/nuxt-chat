ad5715cd4cafde3a331d4068bc28b8b174e802bf
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-06-22 09:07:01 +0900

    トップ画像挿入

--- nuxt-app/components/Header.vue
+++ nuxt-app/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>チャ。</h1>
     <p><span>ー</span>地球の運動と腸内環境、及び発毛について<span>ー</span></p>
+    <img src="/sakana.avif" alt="トップ画像" />
   </header>
 </template>
 
@@ -9,14 +10,20 @@
 header {
   text-align: center;
   font-family: serif;
-  padding-top: 50px;
+  padding-top: 30px;
 }
 header h1 {
-  font-size: 200px;
+  font-size: 150px;
   padding-left: 120px;
+  letter-spacing: -10px;
 }
 header p {
   letter-spacing: 1px;
+}
+header img {
+  margin: auto;
+  max-width: 500px;
+  margin-top: 50px;
 }
 
 header span {
@@ -27,6 +34,9 @@ header span {
   header h1 {
     font-size: 100px;
     padding-left: 50px;
+  }
+  header img {
+    max-width: 100%;
   }
 }
 header p {
