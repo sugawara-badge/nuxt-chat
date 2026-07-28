732fe73eaaa304087c3f7a7fc66661ecd6b5d2a6
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-07-28 09:44:08 +0900

    authリソース作成

--- backend/README.md
+++ backend/README.md
@@ -3,3 +3,6 @@ docker compose up
 
 npx prisma migrate dev --name init
 npx prisma migrate reset
+
+1. authリソース作成
+   nest g resource auth
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileController } from './profile/profile.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController, ProfileController],
   providers: [AppService],
 })
