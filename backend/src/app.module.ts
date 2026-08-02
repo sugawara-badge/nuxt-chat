07b1666c851dba618702ee3594b40ca91fb7a837
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-08-02 12:41:47 +0900

    prismaモジュールなどを作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileController } from './profile/profile.controller';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController, ProfileController],
   providers: [AppService],
 })
