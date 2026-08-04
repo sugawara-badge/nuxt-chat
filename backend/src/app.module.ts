581aefdbaab216e43780ddee00b51b1509010762
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-08-05 08:27:19 +0900

    googleリソース作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { ProfileController } from './profile/profile.controller';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { GoogleModule } from './google/google.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, PrismaModule, GoogleModule],
   controllers: [AppController, ProfileController],
   providers: [AppService],
 })
