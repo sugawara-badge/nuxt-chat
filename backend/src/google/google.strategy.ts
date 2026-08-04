cd23803f90d754c365c6ed3633c2b9206eda82d3
badge-sugawara <badge-sugawara@badge-sugawaranoMacBook-Pro.local>
2026-08-05 08:41:25 +0900

    型修正

--- backend/src/google/google.strategy.ts
+++ backend/src/google/google.strategy.ts
@@ -10,8 +10,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     // Passportのstrategyに関する設定
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       callbackURL: process.env.CALLBACK_URL,
       scope: ['email', 'profile'],
     });
